86a565ebb4007331d9a92bf907542ec297c1a8ea
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 01:41:33 +0900

    Add vocabulary and sentence models

--- difficulty_analysis/admin.py
+++ difficulty_analysis/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vocabulary, Sentence
+
+@admin.register(Vocabulary)
+class VocabularyAdmin(admin.ModelAdmin):
+    list_display = ('word', 'level', 'created_at', 'updated_at', 'used_status')
+    pass
+
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'memo', 'created_at', 'updated_at', 'used_status')
+    pass
--- difficulty_analysis/models.py
+++ difficulty_analysis/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Vocabulary(models.Model):
+    word = models.CharField(max_length=128, default='')
+    level = models.BigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    used_status = models.BooleanField(default=False)
+
+
+class Sentence(models.Model):
+    text = models.TextField(max_length=100000, default='')
+    memo = models.CharField(max_length=1024, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    used_status = models.BooleanField(default=False)
