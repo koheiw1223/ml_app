09dc65b39b0f15b0449529bb06545a3e96f4a2d1
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 08:44:40 +0900

    Add verbose name (models)

--- difficulty_analysis/admin.py
+++ difficulty_analysis/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Vocabulary, Sentence
+from .models import Tag, Vocabulary, Sentence
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    pass
 
 @admin.register(Vocabulary)
 class VocabularyAdmin(admin.ModelAdmin):
-    list_display = ('word', 'level', 'created_at', 'updated_at', 'used_status')
+    list_display = ('word', 'get_tags', 'level', 'created_at', 'updated_at', 'used_status')
     pass
+
+    def get_tags(self, obj):
+         return "\n, ".join([tag.name for tag in obj.tags.all()])
 
 @admin.register(Sentence)
 class SentenceAdmin(admin.ModelAdmin):
--- difficulty_analysis/models.py
+++ difficulty_analysis/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=32, verbose_name='タグ名')
+    created_at = models.DateTimeField(auto_now_add=True,  verbose_name='作成日')
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.created_at)
+
 class Vocabulary(models.Model):
-    word = models.CharField(max_length=128, default='')
-    level = models.BigIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    used_status = models.BooleanField(default=False)
+    word = models.CharField(max_length=128, default='', verbose_name='単語/語彙')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='タグ')
+    level = models.BigIntegerField(verbose_name='難易度')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日')
+    used_status = models.BooleanField(default=False, verbose_name='利用可否')
 
 class Sentence(models.Model):
-    title = models.CharField(max_length=128, default='')
-    text = models.TextField(max_length=100000, default='')
-    memo = models.CharField(max_length=1024, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    used_status = models.BooleanField(default=False)
+    title = models.CharField(max_length=128, default='', verbose_name='タイトル')
+    text = models.TextField(max_length=100000, default='', verbose_name='文章')
+    memo = models.CharField(max_length=1024, default='', verbose_name='メモ')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日')
+    used_status = models.BooleanField(default=False, verbose_name='利用可否')
+
