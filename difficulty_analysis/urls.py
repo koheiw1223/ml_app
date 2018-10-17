67d9cd0904167eb1dc5ca03f9322cc0ee28854b8
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 08:11:57 +0900

    Add sentences page

--- difficulty_analysis/urls.py
+++ difficulty_analysis/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vocabularies/', views.difficulty_analysis, name='vocabularies'),
+    path('vocabularies/', views.vocabulary_index, name='vocabularies'),
+    path('sentences/', views.sentence_index, name='sentences'),
 ]
--- difficulty_analysis/views.py
+++ difficulty_analysis/views.py
@@ -4,8 +4,14 @@ from .models import Vocabulary, Sentence
 def index(request):
     return render(request, "difficulty_analysis/index.html")
 
-def difficulty_analysis(request):
+def vocabulary_index(request):
     d = {
         'vocabularies': Vocabulary.objects.filter(used_status=True),
     }
     return render(request, "difficulty_analysis/vocabulary_index.html", d)
+
+def sentence_index(request):
+    d = {
+        'sentences': Sentence.objects.filter(used_status=True),
+    }
+    return render(request, "difficulty_analysis/sentence_index.html", d)
