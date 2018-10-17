d736a57ced5ad572c205a97ec3f119421850cdc0
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 08:06:26 +0900

    Add page(vocabularies index)

--- difficulty_analysis/urls.py
+++ difficulty_analysis/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vocabularies/', views.difficulty_analysis, name='vocabularies'),
 ]
--- difficulty_analysis/views.py
+++ difficulty_analysis/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Vocabulary, Sentence
 
-# Create your views here.
 def index(request):
-    data = {
-        'article': Vocabulary.objects.filter(used_status=True),
+    return render(request, "difficulty_analysis/index.html")
+
+def difficulty_analysis(request):
+    d = {
+        'vocabularies': Vocabulary.objects.filter(used_status=True),
     }
-    return render(request, "difficulty_analysis/vocabulary_index.html", data)
+    return render(request, "difficulty_analysis/vocabulary_index.html", d)
