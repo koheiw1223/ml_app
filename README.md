bb6c1b812d3747e02314e091908d8794ce730e4a
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 01:06:04 +0900

    Setting environment

--- README.md
+++ README.md
@@ -3,7 +3,23 @@ ml_app
 
 ## Django memo
 
+##### create
+
 ```
 $ sudo docker-compose run ml_app django-admin.py startproject ml_app .
+```
 
+ - [check list](https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/)
+
+##### start
+ 
+```
+$ docker-compose up
+```
+
+##### add superuser
+
+```
+$ docker exec -it  ml_app_ml_app_1 /bin/bash
+$ python manage.py createsuperuser
 ```
--- ml_app/settings.py
+++ ml_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k_t+1+dv@c-1^p$w^uyom%3(xpwl)3wr(o846=yvbl@-$dc1#='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
