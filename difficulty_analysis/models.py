ae98993a36d520d3e280be98814ff4e3cc250cfd
t.kusumoto <t.kusumoto@accelia.net>
2018-10-18 22:48:12 +0900

    Add tf-idf

--- difficulty_analysis/models.py
+++ difficulty_analysis/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 
 class Tag(models.Model):
     name = models.CharField(max_length=32, verbose_name='タグ名')
@@ -15,6 +17,18 @@ class Vocabulary(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日')
     used_status = models.BooleanField(default=False, verbose_name='利用可否')
+
+
+docs = np.array([
+        '牛乳        を 買う',
+        'パン         を 買う',
+        'パン         を 食べる',
+        'お菓子       を 食べる',
+        '本           を 買う',
+        'パン と お菓子 を 食べる',
+        'お菓子        を 買う',
+        'パン と パン   を 食べる'
+        ])
 
 class Sentence(models.Model):
     title = models.CharField(max_length=128, default='', verbose_name='タイトル')
@@ -32,4 +46,15 @@ class Sentence(models.Model):
     @classmethod
     def analysis_tf_idf(cls):
         print("TF-IDF分析 : ", cls.pk)
+
+        # 参考　URL : http://ailaby.com/tfidf/
+        # ベクトル化
+        vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
+        vecs = vectorizer.fit_transform(docs)
+        print(vecs.toarray())
+        # クラスタリング
+        clusters = KMeans(n_clusters=2, random_state=0).fit_predict(vecs)
+        for doc, cls in zip(docs, clusters):
+            print(cls, doc)
+
         return str("")
